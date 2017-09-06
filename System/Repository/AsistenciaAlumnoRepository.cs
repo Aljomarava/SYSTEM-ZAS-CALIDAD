acfd33468958ea500fb83c5c2fe86e9bf0f78488
Gianmarco <gians_13segun@hotmail.com>
2017-09-06 12:34:45 -0500

    Reposi

---
+++ System/Repository/AsistenciaAlumnoRepository.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Domain;
+using System.Data.Entity;
+
+namespace Repository
+{
+    public class AsistenciaAlumnoRepository : IAsistenciaAlumnoRepository
+    {
+        private SystemContext _context;
+
+        public AsistenciaAlumnoRepository()
+        {
+            if (_context == null)
+            {
+                _context = new SystemContext();
+            }
+        }
+
+        public void AddAsistencia(AsistenciaAlumno asistenciaAlumno)
+        {
+            _context.asistenciaAlumno.Add(asistenciaAlumno);
+            _context.SaveChanges();
+        }
+
+        public AsistenciaAlumno GetAsistenciaAlumno(int id)
+        {
+            return _context.asistenciaAlumno.Find(id);
+        }
+
+        public IEnumerable<AsistenciaAlumno> GetAsistenciaAlumno(string criterio)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpdateAsistencia(AsistenciaAlumno asistenciaAlumno)
+        {
+            _context.Entry(asistenciaAlumno).State = EntityState.Modified;
+            _context.SaveChanges();
+        }
+    }
+}
---
+++ System/Repository/IAsistenciaAlumnoRepository.cs
@@ -0,0 +1,17 @@
+﻿using Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repository
+{
+   public  interface IAsistenciaAlumnoRepository
+    {
+        IEnumerable<AsistenciaAlumno> GetAsistenciaAlumno(string criterio);
+        AsistenciaAlumno GetAsistenciaAlumno(int id);
+        void AddAsistencia(AsistenciaAlumno asistenciaAlumno);
+        void UpdateAsistencia(AsistenciaAlumno asistenciaAlumno);
+    }
+}
