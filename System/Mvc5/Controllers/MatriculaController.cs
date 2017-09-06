6aa171f8f7855411bfb6b33cd81c35fdf63e4797
ALEXIS JOSE MARIA RAMIREZ VASQUEZ <713054@upn.pe>
2017-09-06 10:24:05 -0500

    matricula controler

---
+++ System/Mvc5/Controllers/MatriculaController.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+using Service;
+
+namespace Mvc5.Controllers
+{
+    public class MatriculaController : Controller
+    {
+
+        private IAlumnoService _alumnoService;
+        private IApoderadoService _apoderadoService;
+        private IMatriculaService _matriculaService;
+        // GET: Matricula
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+
+        public MatriculaController()
+        {
+            if (_alumnoService == null)
+            {
+                _alumnoService = new AlumnoService();
+            }
+            if (_apoderadoService == null)
+            {
+                _apoderadoService = new ApoderadosService();
+            }
+            if (_matriculaService== null)
+            {
+                _matriculaService = new MatriculaService();
+            }
+        }
+
+        public ActionResult BuscarAlumno()
+        {
+            return View();
+        }
+
+        public ActionResult BuscarAlumnoByCodigo(string codigo)
+        {
+            var alumno = _matriculaService.GetMatriculaByCodigo(codigo);
+
+            var result = new
+            {
+                success = alumno != null ? true : false,
+                data = alumno
+            };
+
+            return Json(result, JsonRequestBehavior.AllowGet);
+        }
+
+
+    }
+}
